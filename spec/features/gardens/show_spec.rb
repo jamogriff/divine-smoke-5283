4693889eb357df0babc7eec7ce0071534496a2c2
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-14 11:37:59 -0600

    Small refactor

--- spec/features/gardens/show_spec.rb
+++ spec/features/gardens/show_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe 'garden show page' do
 
   before :each do
-      @garden = Garden.create!(name: "Sunshine Gardens", organic: true)
-      @plot_1 = @garden.plots.create!(number: 10, size: "Big", direction: "East")
-      @plot_2 = @garden.plots.create!(number: 20, size: "small", direction: "West")
-      @plant_2 = @plot_1.plants.create!(name: 'Pumpkin', description: 'Orange', days_to_harvest: 180)
-      @plant_3 = @plot_2.plants.create!(name: 'Apricot', description: 'Yellow', days_to_harvest: 50)
-      @plant_4 = @plot_2.plants.create!(name: 'Blueberry', description: 'Blue', days_to_harvest: 90)
-      @plant_1 = Plant.create!(name: 'Ivy', description: 'An ivy plant', days_to_harvest: 90)
-      @growing_plant_1 = GrowingPlant.create!(plot: @plot_1, plant: @plant_1)
-      @growing_plant_2 = GrowingPlant.create!(plot: @plot_2, plant: @plant_1)
+    @garden = Garden.create!(name: "Sunshine Gardens", organic: true)
+    @plot_1 = @garden.plots.create!(number: 10, size: "Big", direction: "East")
+    @plot_2 = @garden.plots.create!(number: 20, size: "small", direction: "West")
+    @plant_2 = @plot_1.plants.create!(name: 'Pumpkin', description: 'Orange', days_to_harvest: 180)
+    @plant_3 = @plot_2.plants.create!(name: 'Apricot', description: 'Yellow', days_to_harvest: 50)
+    @plant_4 = @plot_2.plants.create!(name: 'Blueberry', description: 'Blue', days_to_harvest: 90)
+    @plant_1 = Plant.create!(name: 'Ivy', description: 'An ivy plant', days_to_harvest: 90)
+    @growing_plant_1 = GrowingPlant.create!(plot: @plot_1, plant: @plant_1)
+    @growing_plant_2 = GrowingPlant.create!(plot: @plot_2, plant: @plant_1)
   end
 
   it 'shows distinct plants from all plots near harvest ordered by number planted' do
