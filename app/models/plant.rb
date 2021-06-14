1fb7e2a1a2bf8407cdc082e51df6736f9d955304
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-14 10:11:34 -0600

    Implement model method in plant

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -2,4 +2,9 @@ class Plant < ApplicationRecord
   has_many :growing_plants
   has_many :plots, through: :growing_plants
 
+  # Since there could be the same plant in two plots, we need to find the
+  # record only in a certain plot
+  def growing_plant(plot_id)
+    self.growing_plants.where("growing_plants.plot_id = ?", plot_id).pluck(:id).first
+  end
 end
--- spec/models/plant_spec.rb
+++ spec/models/plant_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Plant do
     it {should have_many :growing_plants}
     it {should have_many(:plots).through(:growing_plants)}
   end
+
+  describe '#growing_plant' do
+    it 'finds associated record in growing plant database' do
+      @garden = Garden.create!(name: "Sunshine Gardens", organic: true)
+      @plot_1 = @garden.plots.create!(number: 10, size: "Big", direction: "East")
+      @plant_1 = Plant.create!(name: 'Ivy', description: 'An ivy plant', days_to_harvest: 90)
+      @growing_plant_1 = GrowingPlant.create!(plot: @plot_1, plant: @plant_1)
+
+      expect(@plant_1.growing_plant(@plot_1.id)).to eq @growing_plant_1.id
+    end
+  end
 end
