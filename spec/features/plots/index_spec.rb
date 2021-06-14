990810dda7941f9a28b7bfdb3a734af54485d925
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-14 09:59:44 -0600

    Write feature test for User Story 2

--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe 'plot index page' do
     @garden = Garden.create!(name: "Sunshine Gardens", organic: true)
     @plot_1 = @garden.plots.create!(number: 10, size: "Big", direction: "East")
     @plot_2 = @garden.plots.create!(number: 20, size: "small", direction: "West")
-    @plant_1 = @plot_1.plants.create!(name: 'Ivy', description: 'An ivy plant', days_to_harvest: 90)
+    @plant_1 = Plant.create!(name: 'Ivy', description: 'An ivy plant', days_to_harvest: 90)
+    @growing_plant_1 = GrowingPlant.create!(plot: @plot_1, plant: @plant_1)
     @plant_2 = @plot_1.plants.create!(name: 'Pumpkin', description: 'Orange', days_to_harvest: 180)
     @plant_3 = @plot_2.plants.create!(name: 'Apricot', description: 'Yellow', days_to_harvest: 50)
     @plant_4 = @plot_2.plants.create!(name: 'Blueberry', description: 'Blue', days_to_harvest: 90)
@@ -26,4 +27,10 @@ RSpec.describe 'plot index page' do
     end
   end
 
+  it 'can remove a plant from a plot' do
+    visit "/plots"
+    click_link "remove-#{@growing_plant_1.id}"
+    expect(current_path).to eq "/plots"
+    expect(page).not_to have_content @plant_1.name
+  end
 end
