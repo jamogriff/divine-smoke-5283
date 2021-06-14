cf7e472922be4e8297c1ab0ad7a3ce7d1ca1af9b
Jamison Griffith <jamison.griffith@gmail.com>
2021-06-14 10:56:05 -0600

    Adds validated method to return plants from one garden

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,3 +1,7 @@
 class Garden < ApplicationRecord
   has_many :plots
+
+  def plants_near_harvest
+    Plant.joins(:plots).where("plots.garden_id = ?", self.id).where("plants.days_to_harvest < 100").distinct
+  end
 end
